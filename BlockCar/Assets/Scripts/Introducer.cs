e76082a7b6b9623a1e512873a53fcce9a03c5b00
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2020-11-13 10:28:48 -0800

    Created Introducer script.

---
+++ BlockCar/Assets/Scripts/Introducer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+using System;
+
+public class Introducer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        print("Hello, World!");
+    }
+}

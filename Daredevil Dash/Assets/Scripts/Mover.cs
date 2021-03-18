27680e7e7625a82ca1d7eb413e774db54277befe
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2021-03-17 20:59:55 -0700

    Ignoring .DS_Store files and added other things.

--- Daredevil Dash/Assets/Scripts/Mover.cs
+++ Daredevil Dash/Assets/Scripts/Mover.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 
-public class Mover : MonoBehaviour
+namespace DaredevilDash
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Mover : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float xValue = 1.0f;
+        [SerializeField] private float yValue = 1.0f;
+        [SerializeField] private float zValue = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        public void Update()
+        {
+            var y = (float) Math.Sin(Time.time) * yValue;
+            var x = xValue * Time.deltaTime;
+            var z = zValue * Time.deltaTime;
+            
+            transform.Translate(x, y, z);
+        }
     }
 }

97f797bc551e397c461b59cbee5329a96b1242e9
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2021-03-26 11:40:35 -0700

    Updated Mover.

--- Daredevil Dash/Assets/Scripts/Mover.cs
+++ Daredevil Dash/Assets/Scripts/Mover.cs
@@ -1,23 +1,28 @@
-using System;
-
 using UnityEngine;
 
 namespace DaredevilDash
 {
     public class Mover : MonoBehaviour
     {
-        [SerializeField] private float xValue = 1.0f;
-        [SerializeField] private float yValue = 1.0f;
-        [SerializeField] private float zValue = 1.0f;
-
+        [SerializeField] private float moveSpeed = 1.0f;
+        [SerializeField] private float rotationSpeed = 1.0f;
+        
         // Update is called once per frame
         public void Update()
         {
-            var y = (float) Math.Sin(Time.time) * yValue;
-            var x = xValue * Time.deltaTime;
-            var z = zValue * Time.deltaTime;
-            
-            transform.Translate(x, y, z);
+            MovePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var rotationThisFrame = rotationSpeed * Time.deltaTime;
+
+            var zValue = Input.GetAxis("Vertical") * movementThisFrame;
+            var yValue = Input.GetAxis("Horizontal") * rotationThisFrame;
+
+            transform.Translate(0, 0, zValue);
+            transform.Rotate(0, yValue, 0);
         }
     }
 }
